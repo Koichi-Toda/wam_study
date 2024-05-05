b24ccbd8cf210fd1d6832f0a926e0361523b1f50
土田行一 <toda@tsuchitakouichinoMacBook-Pro.local>
2024-05-05 21:17:04 +0900

    change and fixed parmanent var detecting function

--- wam_in_prolog/L2/L2_compiler.pl
+++ wam_in_prolog/L2/L2_compiler.pl
@@ -60,7 +60,7 @@ prolog(Code,Query):-
 :- dynamic reg_h/1, reg_s/1, reg_p/1, reg_cp/1, reg_e/1.
 :- dynamic reg_ax/2.
 :- dynamic unify_mode/1.
-:- dynamic parmanent_var_list/1.
+:- dynamic y_cnt/1.
 
 % HEAP is part of main memory (all memory accsess can be use store/2 ).
 set(heap(ADDRESS,VALUE)):- assert(store(ADDRESS,VALUE)).
@@ -141,11 +141,13 @@ wam_exec:-
 
 increment(reg_p(N)):- retract(reg_p(N)), N1 is N + 1, assert(reg_p(N1)).
 increment(reg_h(N)):- retract(reg_h(N)), N1 is N + 1, assert(reg_h(N1)).
+increment(y_cnt(N)):- retract(y_cnt(N)), N1 is N + 1, assert(y_cnt(N1)).
 update(reg_p(N)):- retract(reg_p(_)), assert(reg_p(N)).
 update(reg_cp(N)):- retract(reg_cp(_)), assert(reg_cp(N)).
 update(reg_e(N)):- retract(reg_e(_)), assert(reg_e(N)).
 update(reg_ax(AX,VALUE)):- (retract(reg_ax(AX,_));true), assert(reg_ax(AX,VALUE)).
 update(reg_s(N)):- (retract(reg_s(_)); true), assert(reg_s(N)).
+update(y_cnt(N)):- (retract(y_cnt(_)); true), assert(y_cnt(N)).
 update(unify_mode(M)):- (retract(unify_mode(_)) ; true), assert(unify_mode(M)).
 update(stack(ADDRESS,VALUE)):- (retract(store(ADDRESS,_)); true), assert(store(ADDRESS,VALUE)).
 
@@ -342,25 +344,25 @@ compile_codes([T|TL],[L|LL]):-
 
 compile_code(Head:-Body,L2):-
     assert(count(0)),
+    assert(y_cnt(1)),
 
     % parmanent 変数のリストアップ
     vlist(Head:-Body,VLIST),
-    find_parmanent(VLIST,ParmList), writeln(parmanent_var_list(ParmList)),
-    assert(parmanent_var_list(ParmList)),
+    find_parmanent(VLIST,ParmList), writeln(parm_list(ParmList)),
 
-    terms_top(Head,_,ReOrdered),
-    reg_assign(ReOrdered,RetL,[],LLL),!,writeln(head(reg_assign(ReOrdered,RetL,[],LLL))),
+    terms_top(ParmList,Head,_,ReOrdered),
+    reg_assign(ParmList,ReOrdered,RetL,[],LLL),!,writeln(head(reg_assign(ReOrdered,RetL,[],LLL))),
     var_assign(RetL,L,[],LLL2),writeln(head(var_assign(RetL,L,[],LLL2))),
 
     Vin = LLL2,
-    compile_body(Vin,_Vout,Body,WAM_Body),
+    compile_body(ParmList,Vin,_Vout,Body,WAM_Body),
     append(L,WAM_Body,L2_),
     append(L2_,[proceed],L2).
 
 compile_code(T,L):-
     set_counter(0),
-    terms_top(T,_,ReOrdered),
-    reg_assign(ReOrdered,RetL,[],_),!,
+    terms_top([],T,_,ReOrdered),
+    reg_assign([],ReOrdered,RetL,[],_),!,
     var_assign(RetL,L_,[],_),
     append(L_,[proceed],L).
 
@@ -373,24 +375,24 @@ compile_code(T,L):-
 %    compile_body(Body,_BL),
 %    compile_query(Body,WAM_Query),writeln(wam_body_(WAM_Query)).
 
-terms_top(X,_,ReOrdered):-
+terms_top(ParmList,X,_,ReOrdered):-
     compound(X),!,
     functor(X,N,_),X =.. [N|S],
-    sub_terms_top(S,ZZ),
+    sub_terms_top(ParmList,S,ZZ),
     pickup_args(ZZ,Args,Subs),
     append(Args,Subs,ReOrdered).
 
-compile_body(Vin,Vout,(A,B),L3):-
-  compile_body(Vin,Vout_,A,L),
+compile_body(ParmList,Vin,Vout,(A,B),L3):-
+  compile_body(ParmList,Vin,Vout_,A,L),
   retract(cnt(_)),assert(cnt(0)),
-  compile_body(Vout_,Vout,B,L2),
+  compile_body(ParmList,Vout_,Vout,B,L2),
   append(L,L2,L3).
 
-compile_body(Vin,Vout,RootPropaty,L):-
+compile_body(ParmList,Vin,Vout,RootPropaty,L):-
   functor(RootPropaty,Fname,Arity), RootPropaty =.. [Fname|S],
   body_args(S,S_OUT),
-  body_deep_trans(Vin,S_OUT,S_OUT_D,VL),
-  !,body_xreg_assign(S_OUT_D,S_OUT_D2,VL,_),!,
+  body_deep_trans(ParmList,Vin,S_OUT,S_OUT_D,VL),
+  !,body_xreg_assign(ParmList,S_OUT_D,S_OUT_D2,VL,_),!,
   body_var_assign(S_OUT_D2,S_OUT_D3,VL,_),
   Vout = VL,
   append(S_OUT_D3,[call(Fname/Arity)],L).
@@ -404,72 +406,75 @@ body_args_(X,(X,a:C)):-
   reg_cnt(C).
 
 
-body_deep_trans(Vin,[],[],Vin).
-body_deep_trans(Vin,[X|Z],[X2|Z2],VL):-
-  body_deep_trans_(Vin,X,X2,Vin_),
-  body_deep_trans(Vin_,Z,Z2,VL).
+body_deep_trans(_ParmList,Vin,[],[],Vin).
+body_deep_trans(ParmList,Vin,[X|Z],[X2|Z2],VL):-
+  body_deep_trans_(ParmList,Vin,X,X2,Vin_),
+  body_deep_trans(ParmList,Vin_,Z,Z2,VL).
 
-body_deep_trans_(Vin,(V,Arg),put_value(x:Index,Arg),Vout):-
+body_deep_trans_(ParmList,Vin,(V,Arg),put_value(REG:Index,Arg),Vout):-
   body_find_var(V,Vin,Index),
-  Vout = Vin.
-body_deep_trans_(Vin,(V,Arg),put_variable(x:C,Arg),[vpair(C,V)|Vin]):-
-  var(V),!,counter(C).
-body_deep_trans_(Vin,(X,Arg),Z,[]):- %%% TODO: ここが [] なのが、意味が分かっていない、Vinに置き換える必要があるとおもいいつつ。。。
+  Vout = Vin,writeln(chekichek____(V,ParmList)),
+  (vmember(V,ParmList), writeln(incincincre_b1_reflaction(Index)), REG=y ; REG=x).
+body_deep_trans_(ParmList,Vin,(V,Arg),put_variable(REG:C,Arg),[vpair(C,V)|Vin]):-
+  var(V),!,writeln(chekichek(V,vin(Vin),ParmList)),
+  (vmember(V,ParmList),increment(y_cnt(C)), writeln(incincincre_b1(C)), REG=y ; counter(C), REG=x).
+body_deep_trans_(ParmList,Vin,(X,Arg),Z,[]):- %%% TODO: ここが [] なのが、意味が分かっていない、Vinに置き換える必要があるとおもいいつつ。。。
 　writeln(body_deep_trans(Vin)), %%% INSPECT_CODE この辺は、まだちゃんと実装されていないのだろうけど・・・ちょっと後回しにする
   compound(X),!,
   functor(X,Fname,Arity), X =.. [Fname|S],
-  body_sub_terms(S,S_,Reorder),
+  body_sub_terms(ParmList,S,S_,Reorder),
   Z = [reorder(Reorder,[put_structure(Fname/Arity)=Arg|S_])].
-body_deep_trans_(Vin,(X,Arg),(put_structure(X/0,Arg),Vin)):-
+body_deep_trans_(_ParmList,Vin,(X,Arg),(put_structure(X/0,Arg),Vin)):-
   atom(X),!.
 
 
-body_sub_terms([],[],[]).
-body_sub_terms([X|Y],[A|B],Z):-
-  body_terms(X,A,X_),
-  body_sub_terms(Y,B,Y_),
+body_sub_terms(_ParmList,[],[],[]).
+body_sub_terms(ParmList,[X|Y],[A|B],Z):-
+  body_terms(ParmList,X,A,X_),
+  body_sub_terms(ParmList,Y,B,Y_),
   append(X_,Y_,Z).
 
 
 %% body フラット化処理本体
-body_terms(X,assign_var(N/A)=TmpV,Z):-
+body_terms(ParmList,X,assign_var(N/A)=TmpV,Z):-
   compound(X),!,
   functor(X,N,A),X =.. [N|S],
-  body_sub_terms(S,S_,ReOrder),
+  body_sub_terms(ParmList,S,S_,ReOrder),
   Z = [reorder(ReOrder,[put_structure(N/A)=TmpV|S_])].
-body_terms(X,assign_var(X),[]):-
+body_terms(_ParmList,X,assign_var(X),[]):-
   var(X),!.
 
 %% 引数レジスタのために一枚構造がはさまった
-body_xreg_assign([],[],B,B).
-body_xreg_assign([X|Z],Z3,V,V3):-
-  body_reg_assign(X,X2,V,V2),
-  body_xreg_assign(Z,Z2,V2,V3),
+body_xreg_assign(_ParmList,[],[],B,B).
+body_xreg_assign(ParmList,[X|Z],Z3,V,V3):-
+  body_reg_assign(ParmList,X,X2,V,V2),
+  body_xreg_assign(ParmList,Z,Z2,V2,V3),
   append(X2,Z2,Z3).
 
-body_reg_assign(put_variable(A,B),[put_variable(A,B)],V,V).
-body_reg_assign(put_value(A,B),[put_value(A,B)],V,V).
-body_reg_assign([],[],V,V).
-body_reg_assign([reorder(X1,X2)|Y],Z,V,V3):-
-   body_assign_register(X2,A2,V,V1),
-   body_reg_assign(X1,A1,V1,V2),append(A1,A2,A),
-   body_reg_assign(Y,B,V2,V3),append(A,B,Z).
+body_reg_assign(_ParmList,put_variable(A,B),[put_variable(A,B)],V,V).
+body_reg_assign(_ParmList,put_value(A,B),[put_value(A,B)],V,V).
+body_reg_assign(_ParmList,[],[],V,V).
+body_reg_assign(ParmList,[reorder(X1,X2)|Y],Z,V,V3):-
+   body_assign_register(ParmList,X2,A2,V,V1),
+   body_reg_assign(ParmList,X1,A1,V1,V2),append(A1,A2,A),
+   body_reg_assign(ParmList,Y,B,V2,V3),append(A,B,Z).
 
 
-body_assign_register([],[],VL,VL).
-body_assign_register([X|Y],[A|B],VL,Vnew2):-
-  body_reg_match(X,A,VL,Vnew),
-  body_assign_register(Y,B,Vnew,Vnew2).
+body_assign_register(_ParmList,[],[],VL,VL).
+body_assign_register(ParmList,[X|Y],[A|B],VL,Vnew2):-
+  body_reg_match(ParmList,X,A,VL,Vnew),
+  body_assign_register(ParmList,Y,B,Vnew,Vnew2).
 
 
-body_reg_match(put_structure(X),(put_structure(X),x:C),VL,VL):-　reg_cnt(C).
-body_reg_match(assign_var(X),(assign_var(X),x:Index),VL,VL):-　find_var(X,VL,Index).
-body_reg_match(assign_var(X),(assign_var(X),x:C),VL,[vpair(C,X)|VL]):-　reg_cnt(C).
-body_reg_match(put_structure(X)=a:Index,(put_structure(X),a:Index),VL,VL).
-body_reg_match(put_structure(X)=V,(put_structure(X),x:C),VL,VL):-　var(V),!,reg_cnt(C),V = C.
-body_reg_match(put_structure(X)=V,(put_structure(X),x:V),VL,VL).
-body_reg_match(assign_var(X)=V,(assign_var(X),x:C),VL,VL):-　var(V),!,reg_cnt(C),V = C.
-body_reg_match(assign_var(X)=V,(assign_var(X),V),VL,VL).
+body_reg_match(_ParmList,put_structure(X),(put_structure(X),x:C),VL,VL):-　reg_cnt(C).
+body_reg_match(_ParmList,assign_var(X),(assign_var(X),x:Index),VL,VL):-　find_var(X,VL,Index).
+body_reg_match(ParmList,assign_var(X),(assign_var(X),REG:C),VL,[vpair(C,X)|VL]):-
+  (vmember(X,ParmList), increment(y_cnt(C)), writeln(incincincre(C)), REG=y; reg_cnt(C), REG=x).
+body_reg_match(_ParmList,put_structure(X)=a:Index,(put_structure(X),a:Index),VL,VL).
+body_reg_match(_ParmList,put_structure(X)=V,(put_structure(X),x:C),VL,VL):-　var(V),!,reg_cnt(C),V = C.
+body_reg_match(_ParmList,put_structure(X)=V,(put_structure(X),x:V),VL,VL).
+body_reg_match(_ParmList,assign_var(X)=V,(assign_var(X),x:C),VL,VL):-　var(V),!,reg_cnt(C),V = C.
+body_reg_match(_ParmList,assign_var(X)=V,(assign_var(X),V),VL,VL).
 
 
 body_var_assign([],[],VL,VL).
@@ -496,6 +501,8 @@ body_find_var(Var,[_|L],Index):-
 body_find_var(_,[],_):-
     false.
 
+vmember(X,[Y|_]):- X == Y.
+vmember(X,[_|Y]):- vmember(X,Y).
 
 
 % register counter
@@ -506,37 +513,38 @@ reg_cnt(X):-
 
 
 
-
-
+%% プログラム内変数のリストアップ
+%% SWI prolog の term_variables を使用して変数をピックアップしている
 vlist(Head:-(FirstBody,RestBody),[L|L2]):-
     term_variables((Head,FirstBody),L),
     vlist_body(RestBody,L2).
-
 vlist(_Head:-_SingleBody,[]).
-
 vlist_body((A,B),[AL|BL]):-
     term_variables(A,AL),
     vlist_body(B,BL).
-
 vlist_body(LastBody,[L]):-
     term_variables(LastBody,L).
 
-
+%% parmanent 変数のリストアップ
+%% 入力 List は (Head + 最初のBody のユニークな変数リスト), (それ以降のBodyのユニークな変数リスト)... の多重リストである
+%%
+%% これらを比較して、変数が重複する場合、2つ以上の Body に含まれる変数(= parmanent 変数)と判別できる。
+%% 以下では List を flatten して、変数の重複を発見することで、この変数を parmanent 変数と判断するようにしている。
 find_parmanent(List,ParmList):-
   flatten(List,Rest_),
   find_dup(Rest_,ParmList).
+
 find_dup([_|[]],[]).
 find_dup([X|Z],R):-
   fdup(X,Z,R1),
   find_dup(Z,R2),
   append(R1,R2,R).
+
 fdup(_,[],[]).
 fdup(V,[W|_],[V]):-
    V == W.
 fdup(V,[_|Z],R):-
   fdup(V,Z,R).
-
-
 
 
 
@@ -548,49 +556,48 @@ pickup_args([arg_and_sub(A,S)|Z],Args,Subs):-
     append(S,S_,Subs).
 
 
-sub_terms_top([],[]).
-sub_terms_top([X|Y],ZZZ):-
-    terms_top_down(X,_,ZZ),
-    sub_terms_top(Y,ZZ_),
+sub_terms_top(_ParmList,[],[]).
+sub_terms_top(ParmList,[X|Y],ZZZ):-
+    terms_top_down(ParmList,X,_,ZZ),
+    sub_terms_top(ParmList,Y,ZZ_),
     append(ZZ,ZZ_,ZZZ).
 
 % terms_top_down は、述語の引数部分に対応すると理解、
 % このため、引数レジスタを指定するよう変換されると理解
-terms_top_down(X,unify_var(N/A)=_,ZZ):-
+terms_top_down(ParmList,X,unify_var(N/A)=_,ZZ):-
     compound(X),!,
     functor(X,N,A),X =.. [N|S],
-    sub_terms(S,S_,Ret),
+    sub_terms(ParmList,S,S_,Ret),
     counter(C),
     ZZ = [arg_and_sub([[get_structure(N/A,C)|S_]],Ret)].
 
-terms_top_down(X,unify_var(X),ZZ):-
-    var(X),!,
-    counter(C),
-    ZZ = [arg_and_sub([[unify_var(X,_,C)]],[])].
+terms_top_down(_ParmList,X,unify_var(X),ZZ):-
+    var(X),!,counter(AC),
+    ZZ = [arg_and_sub([[unify_var(X,_,AC)]],[])].
 
-terms_top_down(X,unify_var(X/0)=_,ZZ):-
+terms_top_down(_ParmList,X,unify_var(X/0)=_,ZZ):-
     atom(X),!,
     counter(C),
     ZZ = [arg_and_sub([[get_structure(X/0,C)]],[])].
 
 
 %% Frat fomat transform main part
-terms(X,unify_var(N/A)=TmpV,Z):-
+terms(_ParmList,X,unify_var(N/A)=TmpV,Z):-
     compound(X),!,
     functor(X,N,A),X =.. [N|S],
     sub_terms(S,S_,Ret),
     Z = [[get_structure(N/A)=TmpV|S_]|Ret].
 
-terms(X,unify_var(X),[]):-
+terms(_ParmList,X,unify_var(X),[]):-
     var(X),!.
 
-terms(X,unify_var(X/0)=Tmp,[[get_structure(X/0)=Tmp]]):-
+terms(_ParmList,X,unify_var(X/0)=Tmp,[[get_structure(X/0)=Tmp]]):-
     atom(X),!.
 
-sub_terms([],[],[]).
-sub_terms([X|Y],[A|B],Z):-
-    terms(X,A,X_),
-    sub_terms(Y,B,Y_),
+sub_terms(_ParmList,[],[],[]).
+sub_terms(ParmList,[X|Y],[A|B],Z):-
+    terms(ParmList,X,A,X_),
+    sub_terms(ParmList,Y,B,Y_),
     append(X_,Y_,Z).
 
 counter(X):-
@@ -602,32 +609,35 @@ set_counter(X):-
     assert(count(X)).
 
 %% register assignment main part (with reordering register number)
-reg_assign([],[],V,V).
-reg_assign([X|Y],Z,V,V2):-
-    assign_register(X,A,V,V1),
-    reg_assign(Y,B,V1,V2),append(A,B,Z).
+reg_assign(_ParmList,[],[],V,V).
+reg_assign(ParmList,[X|Y],Z,V,V2):-
+    assign_register(ParmList,X,A,V,V1),
+    reg_assign(ParmList,Y,B,V1,V2),append(A,B,Z).
 
-assign_register([],[],VL,VL).
-assign_register([X|Y],[A|B],VL,Vnew2):-
-    reg_match(X,A,VL,Vnew),
-    assign_register(Y,B,Vnew,Vnew2).
+assign_register(_ParmList,[],[],VL,VL).
+assign_register(ParmList,[X|Y],[A|B],VL,Vnew2):-
+    reg_match(ParmList,X,A,VL,Vnew),
+    assign_register(ParmList,Y,B,Vnew,Vnew2).
 
-reg_match(get_structure(X),(get_structure(X),x:C),VL,VL):-　counter(C).
-reg_match(unify_var(X),(unify_var(X),x:Index),VL,VL):-　find_var(X,VL,Index).
-reg_match(unify_var(X),(unify_var(X),x:C),VL,[vpair(C,X)|VL]):-　counter(C).
-reg_match(get_structure(X,A),(get_structure(X),a:A),VL,VL).
-reg_match(get_structure(X)=V,(get_structure(X),x:C),VL,VL):-　var(V),!,counter(C),V = C.
-reg_match(get_structure(X)=V,(get_structure(X),x:V),VL,VL).
+reg_match(_ParmList,get_structure(X),(get_structure(X),x:C),VL,VL):-　counter(C).
+reg_match(_ParmList,unify_var(X),(unify_var(X),x:Index),VL,VL):-　find_var(X,VL,Index).
+reg_match(ParmList,unify_var(X),(unify_var(X),x:C),VL,[vpair(C,X)|VL]):-
+  (vmember(X,ParmList),increment(y_cnt(C)), writeln(incincincre3(C)); counter(C)).
+reg_match(_ParmList,get_structure(X,A),(get_structure(X),a:A),VL,VL).
+reg_match(_ParmList,get_structure(X)=V,(get_structure(X),x:C),VL,VL):-　var(V),!,counter(C),V = C.
+reg_match(_ParmList,get_structure(X)=V,(get_structure(X),x:V),VL,VL).
 
 % さらに以下の一行を追加
-reg_match(unify_var(X,x:Index,A),(unify_var(X),x:Index,a:A),VL,VL):- find_var(X,VL,Index).
+reg_match(_ParmList,unify_var(X,x:Index,A),(unify_var(X),x:Index,a:A),VL,VL):- find_var(X,VL,Index).
 % 以下の一行を追加したが、本当にこれでいいのか？半信半疑、まあ、結果は良好なり。。。
-reg_match(unify_var(X,V,A),(unify_var(X),x:C,a:A),VL,VL):- var(V),!,counter(C).
+reg_match(ParmList,unify_var(X,V,A),(unify_var(X),REG:C,a:A),VL,VL):-
+  var(V),!,
+  (vmember(X,ParmList),increment(y_cnt(C)), writeln(incincincre4(C)), REG=y; counter(C), REG=x), writeln(pippin(X,ParmList,C)).
 
-reg_match(unify_var(X,V,A),(unify_var(X),V,a:A),VL,VL).
-reg_match(unify_var(X)=V,(unify_var(X),x:C),VL,VL):-　var(V),!,counter(C),V = C.
-reg_match(unify_var(X)=V,(unify_var(X),V),VL,VL).
-reg_match(X,X,VL,VL).
+reg_match(_ParmList,unify_var(X,V,A),(unify_var(X),V,a:A),VL,VL).
+reg_match(_ParmList,unify_var(X)=V,(unify_var(X),x:C),VL,VL):-　var(V),!,counter(C),V = C.
+reg_match(_ParmList,unify_var(X)=V,(unify_var(X),V),VL,VL).
+reg_match(_ParmList,X,X,VL,VL).
 
 
 %% varuable assignment main part (firstry, all instaction command will build. and then adjust varuable assignment process.)
@@ -639,8 +649,10 @@ var_assign([X|Y],[A|B],VL,VL__):-
 % if the valuable has already registerd, use 'set_value' otherwise use 'set_variable'
 var_assign((unify_var(V),x:Index,a:A),get_value(x:Index,a:A),VL,VL):-
     find_var(V,VL,Index).
-var_assign((unify_var(V),x:Index,a:A),get_variable(x:Index,a:A),VL,VL_):-
+var_assign((unify_var(V),XY:Index,a:A),get_variable(XY:Index,a:A),VL,VL_):-
     VL_ = [vpair(Index,V)|VL].
+% var_assign((unify_var(V),y:Index,a:A),get_variable(y:Index,a:A),VL,VL_):-
+%    VL_ = [vpair(Index,V)|VL].
 var_assign((unify_var(V),x:Index),unify_value(x:Index),VL,VL):-
     find_var(V,VL,Index).
 var_assign((unify_var(V),x:Index),unify_variable(x:Index),VL,VL_):-
